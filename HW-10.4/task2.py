1c2457837d9535d9a82325c38e342f847c389892
bohdan <lismanuga@gmail.com>
2023-08-29 23:27:07 +0300

    added print

--- HW-10.4/task2.py
+++ HW-10.4/task2.py
@@ -5,7 +5,7 @@ import numpy as np
 
 x = np.arange(15, dtype=np.int64).reshape(3, 5)
 x[1:, ::2] = -99
-x
+print(x)
 # array([[  0,   1,   2,   3,   4],
 #        [-99,   6, -99,   8, -99],
 #        [-99,  11, -99,  13, -99]])
