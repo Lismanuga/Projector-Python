931cbffc9178cb1e5e7bd3a99d6bdc351f7bb62b
bohdan <lismanuga@gmail.com>
2023-09-09 20:53:14 +0300

    finished 1 task

--- HW-14/task1.py
+++ HW-14/task1.py
@@ -1,1 +1,19 @@
-print('hello')
+
+class Country():
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def add(self, country):
+        new_country = Country(
+            self.name + ' ' + country.name,
+            self.population + country.population)
+        return new_country
+
+
+bosnia = Country('Bosnia', 10_000_000)
+herzegovina = Country('Herzegovina', 5_000_000)
+
+bosnia_herzegovina = bosnia.add(herzegovina)
+print(bosnia_herzegovina.name)
+print(bosnia_herzegovina.population)
