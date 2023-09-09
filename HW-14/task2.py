75b8642b176891189822e9ce519b19ed629e6adc
bohdan <lismanuga@gmail.com>
2023-09-09 20:57:23 +0300

    finished 2 task + repair first task

--- HW-14/task2.py
+++ HW-14/task2.py
@@ -4,16 +4,16 @@ class Country():
         self.name = name
         self.population = population
 
-    def add(self, country):
+    def __add__(self, other_country):
         new_country = Country(
-            self.name + ' ' + country.name,
-            self.population + country.population)
+            self.name + ' ' + other_country.name,
+            self.population + other_country.population)
         return new_country
 
 
 bosnia = Country('Bosnia', 10_000_000)
 herzegovina = Country('Herzegovina', 5_000_000)
 
-bosnia_herzegovina = bosnia.add(herzegovina)
+bosnia_herzegovina = bosnia + herzegovina
 print(bosnia_herzegovina.name)
 print(bosnia_herzegovina.population)
