84f32525a366949879b49a96a3760c1c996befa1
bohdan <lismanuga@gmail.com>
2023-09-09 19:39:52 +0300

    finished 3 task

--- HW-13/task3.py
+++ HW-13/task3.py
@@ -1,7 +1,20 @@
 
 def check_types(func):
     def wrapper(*args, **kwargs):
-        pass
+        annotations = func.__annotations__
+        for arg_name, expected_type in annotations.items():
+            if arg_name != 'return' and arg_name in kwargs:
+                arg_value = kwargs[arg_name]
+                if not isinstance(arg_value, expected_type):
+                    raise TypeError(f"Argument {arg_name} must be {expected_type.__name__}, not {type(arg_value).__name__}")
+        result = func(*args, **kwargs)
+        if 'return' in annotations:
+            expected_return_type = annotations['return']
+            if not isinstance(result, expected_return_type):
+                raise TypeError(f"Arqument must be int not {type(result).__name__}")
+
+        return result
+
     return wrapper
 
 
@@ -10,5 +23,8 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
-add(1, 2)
-add("1", "2")
+print(add(1, 2))
+try:
+    add("1", "2")
+except TypeError as e:
+    print(e)
