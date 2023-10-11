09e126ef4c8b4d07709f2fbe87813a2d26093cdc
bohdan <lismanuga@gmail.com>
2023-10-11 20:09:43 +0300

    fixed hw

--- HW-20/task2.py
+++ HW-20/task2.py
@@ -6,8 +6,8 @@ df = pd.read_csv('dataset.csv')
 selected_columns = df[['Team', 'Yellow Cards', 'Red Cards']]
 print(selected_columns)
 
-num_teams = len(df)
-print("Number of teams participated in Euro2012:", num_teams)
+unique_teams = df['Team'].nunique()
+print("Number of unique teams in Euro2012:", unique_teams)
 
 high_scoring_teams = df[df['Goals'] > 6][['Team', 'Goals']]
 print(high_scoring_teams)
